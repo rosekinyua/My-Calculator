ba8158c1b4dfba3585024967dfcb181e00505b9c
rosekinyua <33892599+rosekinyua@users.noreply.github.com>
2018-01-16 11:37:25 +0300

    Add files via upload

---
+++ SwingCalculator.java
@@ -0,0 +1,14 @@
+
+
+	import java.awt.*;
+	import java.awt.event.*;
+	import javax.swing.*;
+	import javax.swing.event.*;
+	public class SwingCalculator {
+	 public static void main(String[] args) {
+	  Calculator frame = new Calculator();
+	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  frame.setVisible(true);
+	  frame.setSize(400, 450);
+	  }
+	} 
